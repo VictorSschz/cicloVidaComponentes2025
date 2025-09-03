bc1b8755ec4ad62eb229198c710d480744ad63cf
VictorSschz <victorssanchez@hotmail.es>
2025-09-03 23:29:57 +0200

    comenzando preparacion de ejemplos de ciclo de vida de los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path:'',
+    component:HomePageComponent
+  },
+  {
+    path:'contact',
+    component:ContactPageComponent
+  },
+  {
+    path:'about',
+    component:AboutPageComponent
+  },
+  {
+    path:'**',
+    redirectTo: ''
+  }
+];
